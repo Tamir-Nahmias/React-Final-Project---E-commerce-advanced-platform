c7caac79b0f9ea81e408347e097b8d378637c487
Tamir-Nahmias <tamir7001@gmail.com>
2025-05-06 15:12:37 +0300

    adding env variable

--- src/fireBase/fireBase.js
+++ src/fireBase/fireBase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtFWA1l03qN1aWym0HpfliO-BOH2hfHBo",
-  authDomain: "lab9firebase-85baf.firebaseapp.com",
-  projectId: "lab9firebase-85baf",
-  storageBucket: "lab9firebase-85baf.firebasestorage.app",
-  messagingSenderId: "653993298703",
-  appId: "1:653993298703:web:4c2495bf9e08a367868c7a",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'lab9firebase-85baf.firebaseapp.com',
+  projectId: 'lab9firebase-85baf',
+  storageBucket: 'lab9firebase-85baf.firebasestorage.app',
+  messagingSenderId: '653993298703',
+  appId: '1:653993298703:web:4c2495bf9e08a367868c7a',
 };
 
 // Initialize Firebase
